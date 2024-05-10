57eda0055d9598c13ef57dffbbdaf2b578e75bfb
toolateralus <joshnjoshgaming@gmail.com>
2024-05-10 14:35:39 -0400

    Refactor debug.hpp and debug.cpp to use StepKind enum for requestedStep

--- include/debug.hpp
+++ include/debug.hpp
@@ -2,18 +2,29 @@
 #include <string>
 #include <vector>
 struct ASTNode;
+
+enum struct StepKind {
+  None,
+  Over,
+  In,
+  Out,
+};
+
 struct Debug {
   Debug() = delete;
   static std::vector<int> breakpoints;
-  static bool stepRequested;
+  static StepKind requestedStep;
   static void InsertBreakpoint(int loc);
   static void RemoveBreakpoint(int loc);
   static void Continue();
-  static void Step() { stepRequested = true; }
+  static void StepOver() { requestedStep = StepKind::Over; }
+  static void StepIn() { requestedStep = StepKind::In; }
+  static void StepOut() { requestedStep = StepKind::Out; }
   
   static void m_setBreakpoint(std::string &line, const std::string &breakpointKey);
   static void m_printScope();
   static void m_stepOver(ASTNode *&owner, ASTNode *&node);
-  
+
+  static void m_stepIn(ASTNode *&node);
   static void WaitForBreakpoint(ASTNode *owner, ASTNode *node);
 };
--- src/debug.cpp
+++ src/debug.cpp
@@ -8,7 +8,7 @@
 #include <sys/select.h>
 
 std::vector<int> Debug::breakpoints = {};
-auto Debug::stepRequested = false;
+auto Debug::requestedStep = StepKind::None;
 
 void Debug::m_setBreakpoint(std::string &line, const string &breakpointKey) {
   string num = "";
@@ -58,8 +58,50 @@ void Debug::m_stepOver(ASTNode *&owner, ASTNode *&node) {
     throw std::runtime_error("Invalid WaitForBreakpoint caller.");
   }
 }
-void Debug::WaitForBreakpoint(ASTNode *owner, ASTNode *node) {
+void Debug::m_stepIn(ASTNode *&node) {
+  Block *blockPtr;
+  if (auto fnCall = dynamic_cast<Call *>(node)) {
+    auto callable =
+        static_cast<Callable_T *>(fnCall->operand->Evaluate().get());
+    if (auto native =
+            dynamic_cast<NativeCallable_T *>(fnCall->operand->Evaluate().get());
+        native != nullptr) {
+      StepOver();
+      return;
+    }
+    blockPtr = callable->block.get();
+  } else if (auto ifStmnt = dynamic_cast<If *>(node)) {
+    blockPtr = ifStmnt->block.get();
+  } else if (auto elseStmnt = dynamic_cast<Else *>(node)) {
+    blockPtr = elseStmnt->block.get();
+  } else if (auto forStmnt = dynamic_cast<For *>(node)) {
+    blockPtr = forStmnt->block.get();
+  } else if (auto rangeFor = dynamic_cast<RangeBasedFor *>(node)) {
+    blockPtr = rangeFor->block.get();
+  } else if (auto objInit = dynamic_cast<ObjectInitializer *>(node)) {
+    blockPtr = objInit->block.get();
+  }
+  
+  if (!blockPtr) {
+    StepOver();
+    return;
+  }
+  
+  if (blockPtr->statements.size() == 0) {
+    StepOver();
+    return;
+  }
 
+  auto &firstStmnt = blockPtr->statements[0];
+  auto loc = firstStmnt->srcInfo.loc;
+  InsertBreakpoint(loc);
+  requestedStep = StepKind::None;
+}
+void Debug::WaitForBreakpoint(ASTNode *owner, ASTNode *node) {
+  static const string breakpointKey = "breakpoint:";
+  
+  requestedStep = StepKind::None;
+  
   bool match = false;
   int breakpoint;
   for (const auto &loc : breakpoints) {
@@ -68,17 +110,17 @@ void Debug::WaitForBreakpoint(ASTNode *owner, ASTNode *node) {
       breakpoint = loc;
     }
   }
-
+  
   if (!match) {
     return;
   }
-
+  
   std::cout << "breakpoint hit : line:" << breakpoint << "\n" << std::flush;
   std::cout << "node: " << typeid(*node).name() << "\n";
   
-  while (!stepRequested && breakpoint == node->srcInfo.loc) {
+  while (requestedStep == StepKind::None && breakpoint == node->srcInfo.loc) {
     usleep(100'000);
-
+    
     fd_set set;
     struct timeval timeout;
     FD_ZERO(&set);
@@ -90,70 +132,39 @@ void Debug::WaitForBreakpoint(ASTNode *owner, ASTNode *node) {
       if (std::getline(std::cin, line)) {
         
         if (line == "over") {
-          stepRequested = true;
+          m_stepOver(owner, node);
           break;
         }
+        
         if (line == "in") {
-          
-          Block *blockPtr;
-          if (auto fnCall = dynamic_cast<Call*>(node)) {
-            auto callable = static_cast<Callable_T*>(fnCall->operand->Evaluate().get());
-            if (auto native = dynamic_cast<NativeCallable_T*>(fnCall->operand->Evaluate().get()); native != nullptr) {
-              stepRequested = true;
-              break;
-            }
-            blockPtr = callable->block.get();
-          }
-          else if (auto ifStmnt = dynamic_cast<If *>(node)) {
-            blockPtr = ifStmnt->block.get();
-          } else if (auto elseStmnt = dynamic_cast<Else *>(node)) {
-            blockPtr = elseStmnt->block.get();
-          } else if (auto forStmnt = dynamic_cast<For *>(node)) {
-            blockPtr = forStmnt->block.get();
-          } else if (auto rangeFor = dynamic_cast<RangeBasedFor *>(node)) {
-            blockPtr = rangeFor->block.get();
-          } else if (auto objInit = dynamic_cast<ObjectInitializer *>(node)) {
-            blockPtr = objInit->block.get();
-          }
-          
-          if (!blockPtr) {
-            stepRequested = true;
-            break;
-          }
-          
-          if (blockPtr->statements.size() == 0) {
-            stepRequested = true;
-            break;
-          }
-          
-          auto &firstStmnt = blockPtr->statements[0];
-          auto loc = firstStmnt->srcInfo.loc;
-          InsertBreakpoint(loc);
+          m_stepIn(node);
           break;
         } 
         
-        static const string breakpointKey = "breakpoint:";
-        if (line.length() > breakpointKey.length() &&
+        else if (line.length() > breakpointKey.length() &&
             line.substr(0, breakpointKey.length()) == breakpointKey) {
           m_setBreakpoint(line, breakpointKey);
           continue;
         }
-        
-        if (line == "continue") {
-          return;
+        else if (line == "continue") {
+          Continue();
+          break;
         }
-        if (line == "inspect") {
+        else if (line == "inspect") {
           m_printScope();
         }
       }
     }
   }
   
-  
-  if (stepRequested) {
-    stepRequested = false;
-
-    m_stepOver(owner, node);
+  switch(requestedStep) {
+  case StepKind::Over:
+  m_stepOver(owner, node);
+  case StepKind::In:
+  m_stepIn(node);
+  case StepKind::None:
+  case StepKind::Out:
+    break;
   }
 }
 void Debug::RemoveBreakpoint(int loc) {
@@ -161,8 +172,9 @@ void Debug::RemoveBreakpoint(int loc) {
       std::remove(breakpoints.begin(), breakpoints.end(), loc);
   breakpoints.erase(removed, breakpoints.end());
 }
-void Debug::Continue() { stepRequested = false; }
+void Debug::Continue() { requestedStep = StepKind::None; }
+
 void Debug::InsertBreakpoint(int loc) {
   breakpoints.push_back(loc);
-  stepRequested = false;
+  requestedStep = StepKind::None;
 }
